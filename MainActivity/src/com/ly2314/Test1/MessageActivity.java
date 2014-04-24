47f2f5c938eb2c1c1f9bd739a50d3098f020e5fd
Ting-Yuan Hsia <ly2314@gmail.com>
2014-04-24 20:35:27 +0800

    Write File
    Using FileOutputStream to write a file.

--- MainActivity/src/com/ly2314/Test1/MessageActivity.java
+++ MainActivity/src/com/ly2314/Test1/MessageActivity.java
@@ -1,12 +1,18 @@
 package com.ly2314.Test1;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MessageActivity extends Activity {
 
 	private TextView _textView;
+	private static final String FILE_NAME = "text.txt";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,28 @@ public class MessageActivity extends Activity {
 		_textView = (TextView) findViewById(R.id.textView1);
 		String text = getIntent().getStringExtra("text");
 		_textView.setText(text);
+		
+		WriteFile(text);
+	}
+	
+	private void WriteFile(String txt)
+	{
+		try
+		{
+			txt += "\n";
+			
+			FileOutputStream fos = openFileOutput(FILE_NAME, Context.MODE_APPEND);
+			fos.write(txt.getBytes());
+			fos.flush();
+			fos.close();
+			
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
